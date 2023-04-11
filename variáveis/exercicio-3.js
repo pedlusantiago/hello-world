b268616f66c44ce4916111b99f43daf172c763d7
Pedro Santiago <pedlusantiago@gmail.com>
2023-04-11 15:02:23 -0300

    exercicios 3 e 4 adicionados

---
+++ variáveis/exercicio-3.js
@@ -0,0 +1,17 @@
+let x = 15, y = 20;
+let resultado = x + y;
+let mensagem = " A soma de " + x + " e " + y + " é " + resultado;
+console.log(mensagem);
+
+resultado = x - y;
+mensagem = " A subtração de " + x + " e " + y + " é " + resultado;
+console.log(mensagem);
+
+
+resultado = x * y;
+mensagem = " A multiplicação de " + x + " e " + y + " é " + resultado;
+console.log(mensagem);
+
+resultado = x / y;
+mensagem = " A divisão de " + x + " por " + y + " é " + resultado;
+console.log(mensagem)
---
+++ variáveis/exercicio-4.js
@@ -0,0 +1,14 @@
+let x = 15, y = 20;
+
+let resultado = x + y;
+console.log(`A soma de ${x} e ${y} é ${resultado}`);
+
+resultado = x - y;
+console.log(mensagem);
+
+
+resultado = x * y;
+console.log(mensagem);
+
+resultado = x / y;
+console.log(mensagem)
